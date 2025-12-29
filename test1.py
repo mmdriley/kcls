2f27001f1576ef2e9bdcffc012034b5149d40231
Matthew Riley <mdriley@gmail.com>
2025-12-28 21:52:10 -0600

    Support iterating over multiple user logins

--- test1.py
+++ test1.py
@@ -60,20 +60,24 @@ def checked_out_items(driver: RemoteWebDriver) -> Iterable[CheckedOutItem]:
     author = t('a[data-key=author-link]')  # sometimes missing
 
     due_date_str = t('div.cp-checked-out-due-on').removeprefix('Due by ')
-    due_date = datetime.strptime(due_date_str, '%b %d, %Y')
+    due_date = datetime.strptime(due_date_str, '%b %d, %Y')  # e.g. Dec 29, 2025
 
     barcode = t('div.cp-barcode-field span.field-value')
 
     yield CheckedOutItem(title, author, barcode, due_date)
 
 
-UN = os.environ['KCLS_USERNAME']
-PW = os.environ['KCLS_PASSWORD']
+creds = os.environ['KCLS_CREDS']
 
+for cred in creds.splitlines():
+  if not cred:  # e.g. blank line
+    continue
 
-with webdriver.Chrome(options) as driver:
-  login_as(driver, UN, PW)
+  un, pw = cred.split(':', 2)
 
-  for it in checked_out_items(driver):
-    print(f'{it.due_date:%Y-%m-%d} {it.barcode:<20}')
-    print(f'  {it.title:>40} {it.author:>20}')
+  with webdriver.Chrome(options) as driver:
+    login_as(driver, un, pw)
+
+    for it in checked_out_items(driver):
+      print(f'{it.due_date:%Y-%m-%d} {it.barcode:<20}')
+      print(f'  {it.title:>40} {it.author:>20}')
