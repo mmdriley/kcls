6415a1d0a872a42410977658ceb38d3e4e0695e4
Matthew Riley <mdriley@gmail.com>
2026-01-17 22:52:02 -0800

    Print for multiple users

--- checkedout.py
+++ checkedout.py
@@ -1,10 +1,26 @@
+import json
+import os
+
 import apiclient
 
 
 def main():
-    client = apiclient.default_client()
-    for item in client.get_checked_out_items():
-        print(f'{item.title} // {item.author} // {item.barcode} // {item.due_date}')
+    if 'KCLS_CREDS' in os.environ:
+        creds = json.loads(os.environ['KCLS_CREDS'])
+    else:
+        creds = [
+            {
+                'username': os.environ['KCLS_USERNAME'],
+                'password': os.environ['KCLS_PASSWORD'],
+            }
+        ]
+
+    for cred in creds:
+        client = apiclient.KCLSClient(cred['username'], cred['password'])
+        print(f'# {cred.get("display_name", cred["username"])}')
+        for item in client.get_checked_out_items():
+            print(f'{item.title} // {item.author} // {item.barcode} // {item.due_date}')
+        print()
 
 
 if __name__ == '__main__':
