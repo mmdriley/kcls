21aafad2976ec0ad977a452f35dd75a91bacb77c
Matthew Riley <mdriley@gmail.com>
2026-01-17 21:56:54 -0800

    Add Ruff config and format source

--- checkedout.py
+++ checkedout.py
@@ -11,22 +11,32 @@ def must[T](v: T | None) -> T:
 
 
 def main():
-    sessions_uri = ('https://gateway.bibliocommons.com/v2/libraries/(libraryName)/sessions'.replace('(libraryName)', 'kcls'))
+    sessions_uri = (
+        'https://gateway.bibliocommons.com/v2/libraries/(libraryName)/sessions'.replace(
+            '(libraryName)', 'kcls'
+        )
+    )
 
-    r = requests.post(sessions_uri, json={
-        'username': os.environ['KCLS_USERNAME'],
-        'password': os.environ['KCLS_PASSWORD'],
-    })
+    r = requests.post(
+        sessions_uri,
+        json={
+            'username': os.environ['KCLS_USERNAME'],
+            'password': os.environ['KCLS_PASSWORD'],
+        },
+    )
     r.raise_for_status()
 
     session_id = r.json()['auth']['sessionId']
     auth_token = r.json()['auth']['authToken']
     # user_id = r.json()['auth']['currentUserId']
 
-    r = requests.get('https://kcls.bibliocommons.com/v2/print/checkedout/out', cookies={
-        'session_id': session_id,
-        'bc_access_token': auth_token,
-    })
+    r = requests.get(
+        'https://kcls.bibliocommons.com/v2/print/checkedout/out',
+        cookies={
+            'session_id': session_id,
+            'bc_access_token': auth_token,
+        },
+    )
     r.raise_for_status()
 
     # we could also be pulling this information from the JSON in
@@ -35,13 +45,16 @@ def main():
     soup = BeautifulSoup(r.text, 'html.parser')
     table = must(soup.select_one('div.cp-print-table table'))
     for row in table.select('tbody tr'):
+
         def t(css: str):
             return must(row.select_one(css)).text
 
         title = t('td.item-title')
         author = t('td.item-author')
         barcode = t('td.item-callnumber p.barcode')
-        due = t('td.item-status div.cp-checked-out-status-overview > div:nth-child(1) > span.field-value')
+        due = t(
+            'td.item-status div.cp-checked-out-status-overview > div:nth-child(1) > span.field-value'
+        )
         due_date = datetime.strptime(due, '%b %d, %Y').date()  # e.g. Dec 29, 2025
 
         print(f'{title} // {author} // {barcode} // {due_date}')
